7da516012c3b7a132cfceb038119385662686114
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2025-07-19 02:11:45 +0530

    commit front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
       
     </>
   )
