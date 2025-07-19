bce3557c32ea267f6964e106722ec93c8c8527ac
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2025-07-19 13:22:46 +0530

    commit first

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import LoginButton from "../components/LoginButton";
 import axios from "axios";
 import Swal from "sweetalert2";
-import back from "../assets/background.jpg";
-import img from "../assets/img1.png"
+import back from "../assets/background.png";
+import img from "../assets/img1.png";
 import { IoPerson } from "react-icons/io5";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { FaEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 export default function Login() {
   const [email, setEmail] = React.useState("");
@@ -18,13 +19,12 @@ export default function Login() {
   const [pass, setPass] = React.useState("password");
 
   const passViewHandler = () => {
-  setPassview((prev) => {
-    const newState = !prev;
-    setPass(newState ? "text" : "password");
-    return newState;
-  });
-};
-
+    setPassview((prev) => {
+      const newState = !prev;
+      setPass(newState ? "text" : "password");
+      return newState;
+    });
+  };
 
   const submitForm = async (e) => {
     e.preventDefault();
@@ -59,51 +59,55 @@ export default function Login() {
 
   return (
     <div
-      className="h-screen flex justify-center items-center"
+      className="h-screen flex flex-col justify-center items-center"
       style={{
         backgroundImage: `url(${back})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-        <img src="" alt="" />
-      <div className="w-[85%] h-[85%] flex rounded-2xl"
-      style={{
-        backgroundImage: `url(${img})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}>
-        
+      <div className="h-16 left-0 w-fit me-auto ms-6">
+        <img src={logo} alt="" className="h-full"/>
+      </div>
+      <div
+        className="w-[85%] h-[85%] flex rounded-2xl shadow-xl"
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="p-5 rounded-e-2xl rounded-s-2xl md:rounded-s-none shadow-md bg-transparent border-[#ffffff]/20 backdrop-blur-[20px] ms-auto w-full md:w-[45%] lg:w-[35%] text-white flex flex-col justify-center">
           <p className="pb-2 text-center text-4xl font-semibold my-8">Login</p>
           <form type="submit" onSubmit={submitForm}>
             <div className="flex gap-2 text-xl my-2 border-b-2 w-full items-center">
-               <IoPerson className="text-white" />
+              <IoPerson className="text-white" />
               <input
                 type="email"
                 placeholder="Enter your mail id"
-                className="outline-none"
+                className="outline-none w-full"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="flex gap-2 text-xl my-2 border-b-2 w-full items-center">
-                <RiLockPasswordFill className="text-white"/>
+              <RiLockPasswordFill className="text-white" />
               <input
                 type={pass}
                 placeholder="Password"
-                className="outline-none"
+                className="outline-none w-full"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <div className="ms-auto hover:cursor-pointer" onClick={passViewHandler}>
-                {
-                    passView ? (<FaRegEye/>):(<FaEyeSlash/>)
-                }
+              <div
+                className="ms-auto hover:cursor-pointer"
+                onClick={passViewHandler}
+              >
+                {passView ? <FaRegEye /> : <FaEyeSlash />}
               </div>
             </div>
             <div className="text-xl my-2 space-x-1">
-              <p className="my-2">Login as:</p>
+              <p className="my-2 ">Login as:</p>
               <input
                 type="radio"
                 id="admin"
@@ -112,7 +116,9 @@ export default function Login() {
                 onClick={() => setLoginType("admin")}
                 className="text-blue-500 hover:cursor-pointer"
               />
-              <label htmlFor="admin" className="hover:cursor-pointer">Admin</label>
+              <label htmlFor="admin" className="hover:cursor-pointer">
+                Admin
+              </label>
               <input
                 type="radio"
                 id="employee"
@@ -121,7 +127,9 @@ export default function Login() {
                 onClick={() => setLoginType("employee")}
                 className="text-blue-500 hover:cursor-pointer"
               />
-              <label htmlFor="employee" className="hover:cursor-pointer">Employee</label>
+              <label htmlFor="employee" className="hover:cursor-pointer">
+                Employee
+              </label>
             </div>
             <div className="text-center">
               <LoginButton name="Login" />
