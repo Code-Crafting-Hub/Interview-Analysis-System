864d1c560d9ce88de18153b5c52fcfaa9df4d27b
kauramadhav50 <159455626+kauramadhav50@users.noreply.github.com>
2025-07-19 14:34:53 +0530

    giving origin

--- backend/Interview_Analysis_system/Interview_Analysis_system/settings.py
+++ backend/Interview_Analysis_system/Interview_Analysis_system/settings.py
@@ -148,6 +148,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 APPEND_SLASH = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
